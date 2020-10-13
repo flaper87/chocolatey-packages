8d2548b0461410e6b93e74aee0f183c239183cbe
Flavio Percoco <flaper87@gmail.com>
2020-10-13 08:27:00 +0100

    Update vault to 1.5.4

--- vault/tools/chocolateyInstall.ps1
+++ vault/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
   Url                 = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_386.zip"
   UnzipLocation       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   Url64               = "https://releases.hashicorp.com/vault/$($version)/vault_$($version)_windows_amd64.zip"
-  Checksum            = '68f8e01d23df3257e6c493a053cc45f3385d4cb8df11e635315a88a0782eb892'
+  Checksum            = 'a3a2e541bdb0f5cb3f6f2abcb9c4bab604692041c2a2c8294dc896f22754b67f'
   ChecksumType        = 'sha256'
-  Checksum64          = '3f1bf837f548a371e3adaf5d8ba95e4ce1f6e0c14e98dda2fa620d51d3844f31'
-  version             = '1.5.2'  
+  Checksum64          = 'a254b5b54fe25b17a069abfff994f5aa884fb75f3575a8d11b76fb95d565e7d0'
+  version             = '1.5.4'
 }
 
 Install-ChocolateyZipPackage @packageArgs
